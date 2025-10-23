f1f9618f05c889ab01d96e625d5bbc5210ff4e50
Lucas Rabello <lucasrabello2020@gmail.com>
2025-10-23 19:29:45 -0300

    Update

--- Curso Udemy/Criando novas rotas/main.go
+++ Curso Udemy/Criando novas rotas/main.go
@@ -15,7 +15,8 @@ func main() {
 	// HandleFunc pois é mais simples nesse caso
 	mux.HandleFunc("/notes/", func(w http.ResponseWriter, r *http.Request){
 		// identificar o metodo da requisição
-		if r.Method != "POST" {
+		if r.Method != "POST" { // pode usar também -> http.MethodPost que é uma melhor forma e mais organizada
+			w.Header().Set("Allow", "POST") // serve para escrever no Header que o único método permitido é o POST
 			// Rejeitar a request
 			w.WriteHeader(405) // código para método não permitido
 			fmt.Fprint(w, "Método usado é diferente de POST, portanto impossivel continuar!")
@@ -25,11 +26,24 @@ func main() {
 	})
 
 	mux.HandleFunc("/notes/view", func(w http.ResponseWriter, r *http.Request) {
+		// segunda forma de negar as requisições
+		if r.Method != http.MethodPost {
+			w.Header().Set("Allow", http.MethodPost) // serve para escrever no Header que o único método permitido é o POST
+			http.Error(w, "Método não permitido!", 405) // Erorr escreve a mensagem de erro junto do status code
+			// ou
+			// http.Error(w, "Método não permitido!", http.StatusMethodNotAllowed) // é a mesma coisa de 405
+		}
 		w.Write([]byte("Você está vendo alguma nota!"))
 	})
 
 	mux.HandleFunc("/notes/create", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Você está criando uma nota!"))
+		w.Header().Set("Content-Type", "application/json") // dizendo no cabeçalho que seŕa uma requisição do tipo json
+		fmt.Fprintf(w, `{"id":"teste em json"}`) // eviando o texto em json
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(200)
+		w.Write([]byte("Rotas: /notes /notes/view  /notes/create"))
 	})
 	
 	http.ListenAndServe(PORTA, mux)
